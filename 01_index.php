8dc369496b4f241e28a73764b2de200a40dfde27
Slam-James <129076048+Slam-James@users.noreply.github.com>
2023-07-20 13:10:37 -0400

    Add files via upload

---
+++ 01_index.php
@@ -0,0 +1,38 @@
+<?php
+//testing comments
+//(code that does not need to be run)
+/*
+multi line comments
+this is also kind of cool.
+*/
+
+//echo - Output strings, numbers, html, ect.
+echo "hello world, ", "Echo, ";
+
+//print - Works like echo, but can only take in a single argument.
+print "123, ";
+
+//print_r() - Print single values and arrays.
+print_r("[1,2,3], ");
+
+//var_dump() - Returns more info like data type and length.
+var_dump("true, ");
+var_dump("Hello My Friend, ");
+
+//var_export() - Similar to var_dump(). Outputs a strubg representation of a variable.
+var_export("Hello");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Conpatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,
+  initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<h1><?php echo "Post one"; ?> </h1>
+</body>
+</html>
